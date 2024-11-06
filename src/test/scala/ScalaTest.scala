e7d31dc592c1da781825208b90f7b593c9e12dbb
Asusgamer40 <78801504+Asusgamer40@users.noreply.github.com>
2024-11-06 15:20:10 +0100

    Game Logic start of Round 1

--- src/test/scala/ScalaTest.scala
+++ src/test/scala/ScalaTest.scala
@@ -9,7 +9,7 @@ class ScalaTest extends AnyWordSpec with Matchers {
   "UIlogic" should {
     "initPlayer" in {
       val playerName = "TestPlayer"
-      val result = controller.setuplogic.initPlayer(playerName)
+      val result = controller.UIlogic.initPlayer(playerName)
       result should include(playerName)
       result should include regex ("((?:Blaue|Feuer|Sau|Brech|Soja|Augen|Rote|Garten),\\s){4}(Blaue|Feuer|Sau|Brech|Soja|Augen|Rote|Garten)")
     }
@@ -17,14 +17,14 @@ class ScalaTest extends AnyWordSpec with Matchers {
     "initGame" in {
       val input = new java.io.ByteArrayInputStream("2\nPlayer1\nPlayer2\n".getBytes)
       Console.withIn(input) {
-        val result = controller.setuplogic.initGame
+        val result = controller.UIlogic.initGame
         result should include("Player1")
         result should include("Player2")
       }
 
     }
     "weightedRandom" in {
-      val result = controller.setuplogic.weightedRandom(weights = Array(20, 18, 16, 14, 12, 10, 8, 6))
+      val result = controller.UIlogic.weightedRandom(weights = Array(20, 18, 16, 14, 12, 10, 8, 6))
       result should include regex ("(Blaue|Feuer|Sau|Brech|Soja|Augen|Rote|Garten)")
     }
   }
