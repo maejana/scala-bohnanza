d1d340bb206b3d4cce8a2f22093377b523cf3b77
maejana <euule03@gmail.com>
2024-11-07 14:27:49 +0100

    rebuild

--- src/main/scala/controller/GameUpdate.scala
+++ src/main/scala/controller/GameUpdate.scala
@@ -1,5 +1,7 @@
 package Test.controller
 
+import Test.Utility
+
 object GameUpdate {
   def gameUpdate(): String = {
     var i = 0
@@ -16,22 +18,7 @@ object GameUpdate {
       println(Nr)
       println(UIlogic.plantSelectString(playingPlayer))
       i = 0
-      while (i < plantCount) {
-        val Line = scala.io.StdIn.readLine().split(" ", 2)
-        val plantCard = Line(0)
-        val fieldNrStr = Line(1)
-        Utility.plantInfo()
-
-        if (UIlogic.isPlantable(playingPlayer, plantCard) && (fieldNrStr == "1" || fieldNrStr == "2" || fieldNrStr == "3")) {
-          val fieldNr = fieldNrStr.toInt
-          gamelogic.plant(plantCard, fieldNr, playingPlayer)
-          gameUpdateLog.append(s"${playingPlayer.name} pflanzt $plantCard auf Feld $fieldNr\n")
-          i += 1
-        } else {
-          println(model.gamedata.errorBeanNotInHand)
-          println(model.gamedata.errorPlantingField + "\n")
-        }
-      }
+      
       var growingFieldText: String =
         s"""
                            ${playingPlayer.playerName}:
--- src/main/scala/controller/Utility.scala
+++ src/main/scala/controller/Utility.scala
@@ -1,8 +1,42 @@
-package controller
+package Test
+import controller.{UIlogic, gamelogic}
+
 
 object Utility {
 
-  def plantInfo(): String{
+  def plantInfo(): Array[String] = {
+    val Line = scala.io.StdIn.readLine().split(" ", 2)
+    if (Line(1) == "1" || Line(1) == "2" || Line(1) == "3") {
+      val fieldNrStr = Line(1)
+      val plantCard = Line(0)
+      val plantInfo: Array[String] = new Array[String](2)
+      plantInfo(0) = plantCard
+      plantInfo(1) = fieldNrStr
+      plantInfo
+      
+    } else {
+      val plantInfoError: Array[String] = new Array[String](2)
+      plantInfoError(0) = model.gamedata.errorBeanNotInHand
+      plantInfoError(1) = model.gamedata errorPlantingField
+    }
+
+  }
+  
+  def plantPreperation(player : model.player): String = {
+    val gameUpdateLog = new StringBuilder()
+    val plantInfo: Array[String] = Utility.plantInfo()
     
+    val plantCard = plantInfo(0)
+    val fieldNrStr = plantInfo(1) 
+    
+    if (UIlogic.isPlantable(player, plantCard)) {
+      val fieldNr = fieldNrStr.toInt
+      gamelogic.plant(plantCard, fieldNr, player)
+      gameUpdateLog.append(s"${player.name} pflanzt $plantCard auf Feld $fieldNr\n")
+      gameUpdateLog.toString
+    } else {
+      model.gamedata.errorBeanNotInHand
+      model.gamedata.errorPlantingField
+    }
   }
 }
