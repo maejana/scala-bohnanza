0d257be550fca370572ae4de165acd20e03d64b6
maejana <euule03@gmail.com>
2024-11-26 14:02:05 +0100

    rebuild state pattern

--- src/main/scala/Bohnanza/controller/plantAmount.scala
+++ src/main/scala/Bohnanza/controller/plantAmount.scala
@@ -7,39 +7,47 @@ import Bohnanza.view
 import scala.collection.mutable.ArrayBuffer
 
 object plantAmount {
-  trait Stragegy {
+  trait Strategy {
     def execute(): Unit
 
     def execute(cards: ArrayBuffer[model.card], player: model.player): Unit
 
   }
 
-  class Strategy1 {
+  class Strategy1 extends Strategy {
 
     override def execute(): Unit = {
       //gamelogic.trade()
 
     }
+    override def execute(cards: ArrayBuffer[model.card], player: model.player): Unit = {
+
+    }
   }
 
-  class Strategy2 {
-
+  class Strategy2 extends Strategy {
     override def execute(cards: ArrayBuffer[model.card], player: model.player): Unit = {
       println(model.gamedata.drawnCardName)
       Utility.plantDrawnCard(player, Utility.selectCardToPlant(cards, player))
 
     }
+    override def execute(): Unit = {
+      //gamelogic.trade()
+    }
   }
 
-    class Strategy3 {
-      override def execute(cards: ArrayBuffer[model.card], player: model.player): Unit = {
-        Utility.plantDrawnCard(player, cards(0))
-        Utility.plantDrawnCard(player, cards(1))
-      }
-
+  class Strategy3 extends Strategy {
+    override def execute(cards: ArrayBuffer[model.card], player: model.player): Unit = {
+      Utility.plantDrawnCard(player, cards(0))
+      Utility.plantDrawnCard(player, cards(1))
     }
 
-
-    var strategy = if (view.playerInput.keyListener() == 0) new Strategy1 else if (view.playerInput.keyListener() == 1) new Strategy2
-    else if (view.playerInput.keyListener() == 2) new Strategy3 else (println("Fehlereingabe"))
+    override def execute(): Unit = {
+      //gamelogic.trade()
+    }
   }
+
+
+  var strategy: Strategy = if (view.playerInput.keyListener() == 0) new Strategy1 else if (view.playerInput.keyListener() == 1) new Strategy2
+  else if (view.playerInput.keyListener() == 2) new Strategy3 else (null)
+}
